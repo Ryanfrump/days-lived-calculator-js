154caf50ef1bc05ef9ab512d64755b8175ff0091
Ryanfrump <ryanfrump@gmail.com>
2024-07-16 11:23:52 -0600

    add calculator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import Calculator from "./Calculator";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>Age in Days Calculator</h1>
+        <p>
+          Given how man years old someone is, this app will tell you how many
+          days old they are.
+        </p>
+      </header>
+      <main>
+        <Calculator />
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Calculator.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Calculator: React.FC = () => {
+  return <></>;
+};
+
+export default Calculator;
