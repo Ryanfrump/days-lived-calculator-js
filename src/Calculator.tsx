9e724bbe63ebc47421b4109975407b30e005923e
Ryanfrump <ryanfrump@gmail.com>
2024-07-17 08:24:18 -0600

    add name input box

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Calculator: React.FC = () => {
-  return <></>;
+  const [name, setName] = useState<string>("");
+
+  return (
+    <>
+      <form>
+        <div>
+          <label htmlFor="...">Name</label>
+          <input
+            type="text"
+            id="name-input"
+            placeholder="Enter name here"
+            value={name}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>): void =>
+              setName(event.target.value)
+            }
+          />
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default Calculator;
