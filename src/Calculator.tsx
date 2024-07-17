895f4892d69e6bcb477dc20a1661a74025a5346a
Ryanfrump <ryanfrump@gmail.com>
2024-07-17 11:42:54 -0600

    Add age input box

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -2,20 +2,42 @@ import React, { useState } from "react";
 
 const Calculator: React.FC = () => {
   const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number | string>();
+
+  const handleAgeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const newAge = event.target.value;
+    if (newAge === "") {
+      setAge("");
+    } else {
+      setAge(Number(newAge));
+    }
+  };
 
   return (
     <>
       <form>
         <div>
-          <label htmlFor="...">Name</label>
+          <label htmlFor="name">Name</label>
           <input
             type="text"
-            id="name-input"
+            id="name"
             placeholder="Enter name here"
             value={name}
             onChange={(event: React.ChangeEvent<HTMLInputElement>): void =>
               setName(event.target.value)
             }
+          />
+        </div>
+        <div>
+          <label htmlFor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            placeholder="Enter age in years"
+            value={age !== undefined ? age : ""}
+            onChange={handleAgeChange}
           />
         </div>
       </form>
